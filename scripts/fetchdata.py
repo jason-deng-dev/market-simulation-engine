55d930a156e4be05f1eef370bfc176664b2f9825
jason-deng <jasondeng.dev@gmail.com>
2026-09-09 19:33:33 +0800

    data: fetch data from yfinance done

---
+++ scripts/fetchdata.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import sys
+
+tickr = "NVDA"
+interval = "1d"
+period = "max"
+
+yf.download(tickr, interval = interval, period = period, auto_adjust = True).to_csv(f"data/{tickr.lower()}_daily.csv")
