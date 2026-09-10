85c6131dce53c5801f080ee1788658f2c50cbc92
jason-deng <jasondeng.dev@gmail.com>
2026-09-10 14:38:59 +0800

    refactor: change Row => Bar

--- include/datafeed.h
+++ include/datafeed.h
@@ -11,7 +11,7 @@
 #include <system_error>
 #include <vector>
 
-struct Row {
+struct Bar {
   std::string date{};
   double close{}, high{}, low{}, open{};
   std::int64_t volume{};
@@ -21,7 +21,7 @@ class DataFeed {
 public:
 
   bool load(const std::string &filePath);
-  bool next(Row &row);
+  bool next(Bar &bar);
 
 private:
   std::vector<char> buffer{}; // owns the data + lifetime
--- src/datafeed.cpp
+++ src/datafeed.cpp
@@ -22,7 +22,7 @@ bool DataFeed::load(const std::string &filePath) {
     char *currStart = buffer.data();
     char *bufEnd = buffer.data() + buffer.size();
 
-    // skip first 3 header rows
+    // skip first 3 header bars
     for (int i = 0; i < 3; ++i) {
       auto found = std::find(currStart, bufEnd, '\n');
       if (found == bufEnd)
@@ -36,32 +36,32 @@ bool DataFeed::load(const std::string &filePath) {
     return true;
   }
 
-  bool DataFeed::next(Row &row) {
+  bool DataFeed::next(Bar &bar) {
     const char *bufEnd = buffer.data() + buffer.size();
 
     if (cursor >= bufEnd) {
       return false;
     }
 
-    // need to grab the values and change row to reflect current row
-    // and then increment cursor to go to next row (could be '\n' or end of
+    // need to grab the values and change bar to reflect current bar
+    // and then increment cursor to go to next bar (could be '\n' or end of
     // buffer)
     const char *lineStart = cursor;
 
     const char *lineEnd = std::find(lineStart, bufEnd, '\n');
     const char *dateEnd = std::find(lineStart, lineEnd, ',');
 
-    row.date.assign(lineStart, dateEnd);
+    bar.date.assign(lineStart, dateEnd);
 
-    auto [ptr1, ec1] = std::from_chars(dateEnd + 1, lineEnd, row.close);
-    auto [ptr2, ec2] = std::from_chars(ptr1 + 1, lineEnd, row.high);
-    auto [ptr3, ec3] = std::from_chars(ptr2 + 1, lineEnd, row.low);
-    auto [ptr4, ec4] = std::from_chars(ptr3 + 1, lineEnd, row.open);
-    auto [ptr5, ec5] = std::from_chars(ptr4 + 1, lineEnd, row.volume);
+    auto [ptr1, ec1] = std::from_chars(dateEnd + 1, lineEnd, bar.close);
+    auto [ptr2, ec2] = std::from_chars(ptr1 + 1, lineEnd, bar.high);
+    auto [ptr3, ec3] = std::from_chars(ptr2 + 1, lineEnd, bar.low);
+    auto [ptr4, ec4] = std::from_chars(ptr3 + 1, lineEnd, bar.open);
+    auto [ptr5, ec5] = std::from_chars(ptr4 + 1, lineEnd, bar.volume);
 
     if (ec1 != std::errc{} || ec2 != std::errc{} || ec3 != std::errc{} ||
         ec4 != std::errc{} || ec5 != std::errc{}) {
-      std::cerr << "extraction of row failed\n";
+      std::cerr << "extraction of bar failed\n";
       return false;
     }
 
