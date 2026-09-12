4017832568470887b155888dafc165afc113da5b
jason-deng <jasondeng.dev@gmail.com>
2026-09-12 12:29:10 +0800

    refactor: add conditions to execute order in Backtest::run

--- src/backtest.cpp
+++ src/backtest.cpp
@@ -1,14 +1,13 @@
-#include <algorithm>
-#include <limits>
-#include <iostream>
 #include "backtest.h"
 #include "datafeed.h"
-#include "strategy.h"
 #include "state.h"
+#include "strategy.h"
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
 
-
-
-void Backtest::run(DataFeed &feed, Strategy & strategy, State& state) {
+void Backtest::run(DataFeed &feed, Strategy &strategy, State &state) {
 
   Bar bar{};
   double maxPrice = -std::numeric_limits<double>::infinity();
@@ -21,20 +20,23 @@ void Backtest::run(DataFeed &feed, Strategy & strategy, State& state) {
     maxPrice = std::max(maxPrice, bar.high);
     minPrice = std::min(minPrice, bar.low);
 
-    // update equityCurve, executions in state
+    bool zeroOrPosEqtyBefore = state.getNetQty() >= 0;
+
+    // execute only if qty > 0 and can afford
+
+    // moving further from flat (increasing position)
+    bool increasing = std::abs(state.getNetQty() + qty) > std::abs(state.getNetQty());
+    if (qty != 0 && (!increasing && std::abs(qty) * bar.open <= state.getCash())) {
+      state.addExecution(bar.date, qty, bar.open, maxPrice, minPrice);
+    }
+    // update equityCurve
     state.addEquity(bar.date, bar.close);
 
+    bool zeroOrPosEqtyAfter = state.getNetQty() >= 0;
 
-     // cannot afford this transaction;
-    if (qty*bar.open > state.getCash()) continue;
-    bool posNetEqtyBefore = state.getNetQty() > 0;
-
-    state.addExecution(bar.date, qty, bar.open, maxPrice, minPrice);
-
-    bool posNetEqtyAfter = state.getNetQty() > 0;
-    
-    // if state.netQty = 0 or changes between neg and pos, reset maxPrice/minPrice
-    if (state.getNetQty()==0 || posNetEqtyAfter != posNetEqtyBefore) {
+    // if state.netQty = 0 or changes between neg and pos, reset
+    // maxPrice/minPrice
+    if (state.getNetQty() == 0 || zeroOrPosEqtyBefore != zeroOrPosEqtyAfter) {
       maxPrice = -std::numeric_limits<double>::infinity();
       minPrice = std::numeric_limits<double>::infinity();
     }
