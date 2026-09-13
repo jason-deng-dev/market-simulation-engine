3d00e8c7854bb1f66f7025464fc49e09a69dd3b4
jason-deng <jasondeng.dev@gmail.com>
2026-09-13 09:49:46 +0800

    feat: fillRecords now fills positionRecords

--- include/analytics.h
+++ include/analytics.h
@@ -2,12 +2,15 @@
 #define ANALYTICS_H
 
 #include "state.h"
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
 #include <limits>
 #include <string>
 #include <vector>
 
 struct ExitRecord {
-  std::string t;
+  std::string time;
   int qty;
   double exitPrice;
   double avgEntryPrice;
@@ -15,7 +18,7 @@ struct ExitRecord {
 struct PositionRecord {
   std::string openTime, closeTime;
   int direction;
-  double entryNotional, exitNotional, mae, mfe;
+  double entryNotional, exitNotional, pnl;
 };
 
 struct TradeCloseInfo {
@@ -43,7 +46,7 @@ struct SharpeInfo {
 
 class Analytics {
   std::vector<ExitRecord> exitRecords;
-  std::vector<PositionRecord> positionRecord;
+  std::vector<PositionRecord> positionRecords;
 
   TradeCloseInfo tradeCloseMetrics{};
 
@@ -61,15 +64,102 @@ class Analytics {
   // CAGR
   double startEquity{};
   double endEquity{};
-
+public: 
   void captureState(const State &state) {
     // Equity<date, equity, netQty>
     const auto &equityCurve = state.getEquityCurve();
-    // Execution<data, qty, price, maxPrice, minPrice>
+    // Execution<date, qty, price, maxPrice, minPrice>
     const auto &executions = state.getExecutions();
+
+    fillRecords(executions);
   }
 
-  
+  void fillRecords(const std::vector<Execution> &executions) {
+    /*
+    new position happens when
+    currPosQty goes from +/- => 0
+    goes from + => - or - => +
+    currPosQty goes from 0 => +/-
+
+    when new position happens
+    1. deal with last position, store in PositionRecords
+    2. set new openTime, entryNotional, currMaxPrice, currMinPrice;
+
+    if posQty changes signs
+    1. exitNotional added until posQty reaches 0
+    2. new posQty is the remainder
+
+    if posQty > 0, long position
+    - order qty > 0 => add to pos (add to entryNotional)
+    - order qty < 0 => reduce pos (add to exitNotional)
+
+    if posQty < 0, short position
+    - order qty < 0 => add to pos (add to entryNotional)
+    - order qty > 0 => reduce pos (add to exitNotional)
+    */
+
+    int posQty = 0;
+    std::string openTime;
+    double entryNotional = 0;
+    double exitNotional = 0;
+
+    auto sign = [](int q) { return (q > 0) - (q < 0); };
+
+    for (std::size_t i = 0; i < executions.size(); ++i) {
+      auto [date, qty, price, maxPrice, minPrice] = executions[i];
+      int posQtyBefore = posQty;
+      int posQtyAfter = posQty + qty;
+
+      // add to position
+      if (sign(posQty) == sign(qty)) { 
+        posQty += qty;
+        entryNotional += qty * price;
+      }
+      // reduce position
+      else {
+        int fillAmount = std::min(std::abs(posQty), std::abs(qty));
+        if (posQty > 0) {
+          posQty -= fillAmount;
+          exitNotional += fillAmount * price;
+        }
+        else {
+          posQty += fillAmount;
+          exitNotional -= fillAmount * price;
+        }
+        
+      }
+
+      // new position (fill old and reset)
+      if (sign(posQtyBefore) != sign(posQtyAfter)) {
+        // record last position (except if this is first position)
+        if (entryNotional != 0) {
+          int direction = posQtyBefore > 0 ? 1 : -1; // 1 if long, -1 if short
+          positionRecords.push_back({
+              openTime,
+              date,
+              direction,
+              entryNotional,
+              exitNotional,
+              exitNotional-entryNotional
+          });
+        }
+        // reset
+        posQty = posQtyAfter;
+        openTime = date;
+        entryNotional = posQty * price;
+        exitNotional = 0;
+      }
+    }
+  }
+
+  void reportPositions() {
+    std::cout << "size:" << positionRecords.size() << '\n';
+    for (auto &p : positionRecords) {
+      std::cout << "Open time:" << p.openTime << " Close time:" << p.closeTime
+                << " Direction:" << p.direction << " Entry:" << p.entryNotional 
+                << " Exit:" << p.exitNotional<< " PNL:"  << p.pnl << '\n';
+    }
+  }
 };
 
 #endif
