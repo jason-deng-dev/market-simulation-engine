a231c8824725e71a7c8a4b7fe686ef2c487bd2bf
jason-deng <jasondeng.dev@gmail.com>
2026-09-13 16:48:46 +0800

    refactor: remove force close account at end of backtest

--- src/backtest.cpp
+++ src/backtest.cpp
@@ -42,13 +42,6 @@ void Backtest::run(DataFeed &feed, Strategy &strategy, State &state) {
       maxPrice = std::max(maxPrice, bar.high);
       minPrice = std::min(minPrice, bar.low);
     }
-
-    // force close account
-    if (state.getNetQty() != 0) {
-      state.addExecution(bar.date, -state.getNetQty(), bar.close, maxPrice,
-                         minPrice);
-    }
-    state.addEquity(bar.date, bar.close);
   }
   std::cout << "Backtest complete\n";
 }
